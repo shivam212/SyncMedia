7f22008466a72778704940c7df01b19d25810980
ShivamSinha <shivamsinha212@gmail.com>
2018-06-03 16:38:37 +0530

    Basic Layout of the player

--- Player/player.py
+++ Player/player.py
@@ -1,1 +1,19 @@
 #will contain code for the player
+import sys
+from PyQt5 import QtWidgets,QtGui
+
+def window():
+    app = QtWidgets.QApplication(sys.argv)
+    w=QtWidgets.QWidget()
+    play=QtWidgets.QPushButton(w)
+    pause=QtWidgets.QPushButton(w)
+    play.setText('Play')
+    pause.setText('Pause')
+    play.move(0,700)
+    pause.move(50,700)
+    w.setWindowTitle('SyncMedia')
+    w.setGeometry(100,100,1024,768)
+    w.show()
+    sys.exit(app.exec_())
+
+window()
