c7b2cac0ddab0489214eb4911617870ed50b2381
ShivamSinha <shivamsinha212@gmail.com>
2018-06-04 12:42:41 +0530

    added play,pause,stop functionality

--- Player/player.py
+++ Player/player.py
@@ -2,18 +2,32 @@
 import sys
 from PyQt5 import QtWidgets,QtGui
 
-def window():
-    app = QtWidgets.QApplication(sys.argv)
-    w=QtWidgets.QWidget()
-    play=QtWidgets.QPushButton(w)
-    pause=QtWidgets.QPushButton(w)
-    play.setText('Play')
-    pause.setText('Pause')
-    play.move(0,700)
-    pause.move(80,700)
-    w.setWindowTitle('SyncMedia')
-    w.setGeometry(100,100,1024,768)
-    w.show()
-    sys.exit(app.exec_())
+class Window(QtWidgets.QWidget):
 
-window()
+    def __init__(self):
+        super().__init__()
+        self.k=1
+        self.init_ui()
+
+    def init_ui(self):
+        self.play=QtWidgets.QPushButton(self)
+        self.stop=QtWidgets.QPushButton(self)
+        self.play.setText('Play')
+        self.stop.setText('Stop')
+        self.play.move(0,700)
+        self.stop.move(80,700)
+        self.setWindowTitle('SyncMedia')
+        self.setGeometry(100,100,1024,768)
+        self.play.clicked.connect(self.play_click)
+        self.show()
+    def play_click(self):
+        if(self.k==1):
+            self.play.setText('Pause')
+            self.k=0
+        elif(self.k==0):
+            self.play.setText('Play')
+            self.k=1
+
+app=QtWidgets.QApplication(sys.argv)
+window=Window()
+sys.exit(app.exec_())
