e87d765502a03a2f63dcdf5fe7d2eb05704a251a
ShivamSinha <shivamsinha212@gmail.com>
2018-06-05 13:48:03 +0530

    added video layout,still to add function to buttons

--- Player/player.py
+++ Player/player.py
@@ -1,8 +1,11 @@
 #will contain code for the player
 import sys
 from PyQt5 import QtWidgets,QtGui
+from PyQt5.QtMultimedia import QMediaContent, QMediaPlayer
+from PyQt5.QtMultimediaWidgets import QVideoWidget
+from PyQt5.QtCore import QDir, Qt, QUrl
 
-class Window(QtWidgets.QWidget):
+class Window(QtWidgets.QMainWindow):
 
     def __init__(self):
         super().__init__()
@@ -19,6 +22,24 @@ class Window(QtWidgets.QWidget):
         self.setWindowTitle('SyncMedia')
         self.setGeometry(100,100,1024,768)
         self.play.clicked.connect(self.play_click)
+        self.mediaPlayer =QMediaPlayer(None, QMediaPlayer.VideoSurface)
+        self.videoWidget = QVideoWidget()
+        self.videoWidget.setGeometry(100,100,500,500)
+        wid =QtWidgets.QWidget(self)
+        self.setCentralWidget(wid)
+        controlLayout=QtWidgets.QHBoxLayout()
+        controlLayout.addWidget(self.play)
+        controlLayout.addWidget(self.stop)
+        layout =QtWidgets.QVBoxLayout()
+
+        layout.addWidget(self.videoWidget)
+        layout.addLayout(controlLayout)
+        wid.setLayout(layout)
+        self.videoWidget.move(500,500)
+        self.mediaPlayer.setMedia(QMediaContent(QUrl.fromLocalFile("/Users/shivam/Downloads/valerie amy.mp4")))
+        self.mediaPlayer.setVideoOutput(self.videoWidget)
+        self.videoWidget.show()
+        self.mediaPlayer.play()
         self.show()
     def play_click(self):
         if(self.k==1):
