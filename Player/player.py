fa492c2ca8ec5eb7ccb9730ed82c8d1603a4886c
ShivamSinha <shivamsinha212@gmail.com>
2018-06-06 21:59:51 +0530

    Video plays when play buttonpushed

--- Player/player.py
+++ Player/player.py
@@ -3,7 +3,7 @@ import sys
 from PyQt5 import QtWidgets,QtGui
 from PyQt5.QtMultimedia import QMediaContent, QMediaPlayer
 from PyQt5.QtMultimediaWidgets import QVideoWidget
-from PyQt5.QtCore import QDir, Qt, QUrl
+from PyQt5.QtCore import QDir, Qt, QUrl, QSize
 
 class Window(QtWidgets.QMainWindow):
 
@@ -17,42 +17,50 @@ class Window(QtWidgets.QMainWindow):
         self.stop=QtWidgets.QPushButton(self)
         self.play.setText('Play')
         self.stop.setText('Stop')
-        self.play.move(0,700)
-        self.stop.move(80,700)
         self.setWindowTitle('SyncMedia')
         menu = self.menuBar()
         file = menu.addMenu(' &File ')
         open =QtWidgets.QAction(QtGui.QIcon('open.png'), '&Open', self)
         file.addAction(open)
         #file.addAction(close)
+
         self.play.clicked.connect(self.play_click)
         self.mediaPlayer =QMediaPlayer(None, QMediaPlayer.VideoSurface)
         self.videoWidget = QVideoWidget()
+
         wid =QtWidgets.QWidget(self)
         self.setCentralWidget(wid)
+        wid.resize(wid.sizeHint())
         controlLayout=QtWidgets.QHBoxLayout()
         controlLayout.addWidget(self.play)
         controlLayout.addWidget(self.stop)
+        extralayout=QtWidgets.QHBoxLayout()
+        self.videoWidget.resize(self.videoWidget.sizeHint())
+        extralayout.addWidget(self.videoWidget)
+
         layout =QtWidgets.QVBoxLayout()
-        layout.addWidget(self.videoWidget)
+
+        layout.addLayout(extralayout)
+        self.videoWidget.update()
         layout.addLayout(controlLayout)
         wid.setLayout(layout)
-        self.videoWidget.move(500,500)
         self.mediaPlayer.setMedia(QMediaContent(QUrl.fromLocalFile("/Users/shivam/Downloads/valerie amy.mp4")))
         self.mediaPlayer.setVideoOutput(self.videoWidget)
         self.videoWidget.show()
-        self.mediaPlayer.play()
+
+
         self.show()
     def play_click(self):
-        if(self.k==1):
+        if(self.k==0):
             self.mediaPlayer.play()
             self.play.setText('Pause')
-            self.k=0
-        elif(self.k==0):
+            self.k=1
+        elif(self.k==1):
             self.mediaPlayer.pause()
             self.play.setText('Play')
-            self.k=1
+            self.k=0
 
 app=QtWidgets.QApplication(sys.argv)
 window=Window()
+
 sys.exit(app.exec_())
